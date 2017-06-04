327c14f5ffce8440ba35e09e852f137a32b13d46
たかます <bloodteam.a.ranger@gmail.com>
2017-06-04 20:25:56 +0900

    タイトル→ステージセレクトの画面遷移まで

---
+++ Assets/StartButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton: MonoBehaviour {
+
+    public void SceneLoad()
+    {
+        Debug.Log("出力");
+        //StageSelect画面に遷移
+        SceneManager.LoadScene("StageSelect");
+    }
+
+}
