0456b4d31fa15a73cb0aa25e76dc45046d2a53c7
たかます <bloodteam.a.ranger@gmail.com>
2017-07-02 23:40:51 +0900

    主に今日の進捗。ブロックを増やした。次回は座標修正

---
+++ Assets/App/Playing/Scripts/AreaGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaGenerator : MonoBehaviour
+{
+
+
+	public GameObject AreaPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	/*
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	*/
+}
---
+++ Assets/App/Playing/Scripts/CopyAreaGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyAreaGenerator : MonoBehaviour
+{
+
+	public GameObject AreaPrefab;
+
+	//行数をインスペクタから指定
+	[SerializeField]
+	private int _row;
+
+	//列数をインスペクタから指定
+	[SerializeField]
+	private int _column;
+
+	[SerializeField]
+	private Transform _masu;
+
+	//エリアを増やすメソッドを書きたい
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject[,] Areas = new GameObject[_column,_row];
+
+		//行
+		for (int i = 0; i < _row; i++)
+		{
+			//列
+			for (int j = 0; j < _column; j++)
+			{
+				Areas[i,j] = Instantiate(AreaPrefab,_masu) as GameObject;
+				Areas[i,j].transform.localPosition = new Vector2(i * 50,j * 50);
+			}
+		}
+	}
+
+}
