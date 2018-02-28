708c680f11267985c7411acdcfd9454ef94dcbb3
MasuCatz <bloodteam.a.ranger@gmail.com>
2018-02-28 22:05:20 +0900

    修正

--- Assets/App/Playing/Scripts/CopyAreaGenerator.cs
+++ Assets/App/Playing/Scripts/CopyAreaGenerator.cs
@@ -32,7 +32,6 @@ public class CopyAreaGenerator : MonoBehaviour
 
 				//ブロック毎に座標情報を付加して命名
 				Areas[i, j].name = "AreaPrefab" + bn;
-				//Areas[i, j].transform.localPosition = new Vector2((i * 50) - (GameManager.Instance.Row * 25) + 25, (j * 50) -(GameManager.Instance.Column * 25) + 25);
 				Areas[i, j].transform.localPosition = new Vector2((i * 50) - (GameManager.Instance.Row * 25) + 25, (j * 50) - (GameManager.Instance.Column * 25) + 25);
 
 				//子オブジェクト生成(周囲の爆弾数を後で記述する)
@@ -42,9 +41,15 @@ public class CopyAreaGenerator : MonoBehaviour
 				bombCounter.GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
 				bombCounter.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
 				bombCounter.GetComponent<Text>().color = new Color(0,0,0,1);
+
+
+				//判定を親オブジェクトと同サイズへ
+				//bombCounter.GetComponent<RectTransform>().sizeDelta = new Vector2(Areas[i, j].GetComponent<RectTransform>().sizeDelta.x, Areas[i, j].GetComponent<RectTransform>().sizeDelta.y);
+
+
+				//テキストコンポーネントの判定を消す
 				bombCounter.GetComponent<Text>().raycastTarget = false;
 
-				//機能としては正しいがクリックの位置が何故かずれる
 				bombCounter.transform.SetParent(Areas[i, j].transform, false);
 				bombCounter.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
 
